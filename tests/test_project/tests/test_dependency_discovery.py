6d509159fca2684415a1ef6a61b942bc4490dafd
Federico Capoano <nemesis@ninux.org>
2017-06-28 17:20:31 +0200

    [tests] Improved dependency discovery tests
    - added test for DependencyLoader
    - renamed classes and file to a generic name

---
+++ tests/test_project/tests/test_dependency_discovery.py
@@ -0,0 +1,16 @@
+import unittest
+
+from openwisp_utils.loaders import DependencyLoader
+from openwisp_utils.staticfiles import DependencyFinder
+
+
+class TestDependencyDiscovery(unittest.TestCase):
+    def test_dependency_finder(self):
+        finder = DependencyFinder()
+        self.assertIsInstance(finder.locations, list)
+        self.assertIn('django_netjsonconfig', finder.locations[0][1])
+
+    def test_dependency_loader(self):
+        loader = DependencyLoader(engine=None)
+        self.assertIsInstance(loader.get_dirs(), list)
+        self.assertIn('django_netjsonconfig', loader.get_dirs()[0])
