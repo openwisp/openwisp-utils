1b9672134363bc00c28e720e29fa094e12c953c7
Federico Capoano <nemesis@ninux.org>
2020-01-07 16:12:31 +0100

    [admin_theme] Improved PR #62

--- openwisp_utils/admin_theme/context_processor.py
+++ openwisp_utils/admin_theme/context_processor.py
@@ -1,11 +1,12 @@
-# TODO: remove once all openwisp modules are using admin_site
-import warnings
+import logging
 
 from django.apps import registry
 from django.conf import settings
 from django.contrib.admin import site
 from django.urls import reverse
 from openwisp_utils.admin_theme.site import admin_site
+
+logger = logging.getLogger(__name__)
 
 
 def menu_items(request):
@@ -25,6 +26,7 @@ def build_menu(request=None):
     custom_items = getattr(settings, 'OPENWISP_ADMIN_MENU_ITEMS', [])
     items = custom_items or default_items
     menu = []
+    log_warning = False
     # loop over each item to build the menu
     # and check user has permission to see each item
     for item in items:
@@ -35,14 +37,20 @@ def build_menu(request=None):
         label = item.get('label', model_class._meta.verbose_name_plural)
         try:
             model_admin = admin_site._registry[model_class]
+        # TODO: remove once all modules are upgraded to the new OpenWISP admin_site
         except KeyError:
             model_admin = site._registry[model_class]
-            warnings.warn('default django admin.site is deprecated by OpenWISP \
-                           please consider moving our to new admin_site')
+            log_warning = True
         if not request or model_admin.has_module_permission(request):
             menu.append({
                 'url': url,
                 'label': label,
                 'class': model.lower()
             })
+    if log_warning:
+        logger.warning(
+            'The default django admin.site is deprecated in OpenWISP; please '
+            'consider moving to the usage of the configurable admin_site '
+            'available in openwisp_utils.admin_theme.site'
+        )
     return menu
