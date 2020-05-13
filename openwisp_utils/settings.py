c28d5be97ece624286609757f34abd53df42c7e8
Pablo Castellano <pablo@anche.no>
2020-05-13 12:04:22 +0200

    [api] Add OPENWISP_API_INFO setting #102
    Related to #102

--- openwisp_utils/settings.py
+++ openwisp_utils/settings.py
@@ -1,4 +1,15 @@
 from django.conf import settings
 
 EXTENDED_APPS = getattr(settings, 'EXTENDED_APPS', [])
+
+# API settings
 REST_SWAGGER = getattr(settings, 'OPENWISP_REST_SWAGGER', True)
+API_INFO = getattr(
+    settings,
+    'OPENWISP_API_INFO',
+    {
+        'title': 'OpenWISP API',
+        'default_version': 'v1',
+        'description': 'OpenWISP REST API',
+    },
+)
