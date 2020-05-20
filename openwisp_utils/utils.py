525dedbb21cc10f92e880c8d263eb1a8dd83bb31
Gagan Deep <the.one.above.all.titan@gmail.com>
2020-05-21 04:08:56 +0530

    [utils] Added deep_merge_dicts (#113)

--- openwisp_utils/utils.py
+++ openwisp_utils/utils.py
@@ -6,3 +6,14 @@ def get_random_key():
     generates a device key of 32 characters
     """
     return get_random_string(length=32)
+
+
+def deep_merge_dicts(dict1, dict2):
+    result = dict1.copy()
+    for key, value in dict2.items():
+        if isinstance(value, dict):
+            node = result.get(key, {})
+            result[key] = deep_merge_dicts(node, value)
+        else:
+            result[key] = value
+    return result
--- tests/test_project/tests/test_test_utils.py
+++ tests/test_project/tests/test_test_utils.py
@@ -1,6 +1,7 @@
 from django.dispatch import Signal
 from django.test import TestCase
 from openwisp_utils.tests import catch_signal
+from openwisp_utils.utils import deep_merge_dicts
 
 status_signal = Signal(providing_args=['status'])
 
@@ -15,3 +16,24 @@ class TestUtils(TestCase):
         handler.assert_called_once_with(
             status='working', sender=self, signal=status_signal,
         )
+
+    def test_deep_merge_dicts(self):
+        dict1 = {
+            'key1': 'value1',
+            'key2': {'key2-1': 'value2-1', 'unchanged': 'unchanged'},
+            'unchanged': 'unchanged',
+        }
+        dict2 = {
+            'key1': 'value1-final',
+            'key2': {'key2-1': 'value2-1-final', 'key2-2': 'value2-2'},
+        }
+        merged = {
+            'key1': 'value1-final',
+            'key2': {
+                'key2-1': 'value2-1-final',
+                'key2-2': 'value2-2',
+                'unchanged': 'unchanged',
+            },
+            'unchanged': 'unchanged',
+        }
+        self.assertDictEqual(deep_merge_dicts(dict1, dict2), merged)
