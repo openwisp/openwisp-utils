02ff52e0747b2177548a2507e81ab46aa8fb61a3
Arshpreet Singh <arsharsh2391@gmail.com>
2021-03-03 10:55:12 +0530

    [openwisp_utils/storage] Added a Custom Static storage class #166
    Ported changes from  storage.py in ansible-openwisp2 to storage.py in openwisp_utils/.
    Created test_storage in tests/test_project/tests , The test creates a temporary folder and 2 text files in it and then runs collectstatic in that folder and checks whether the file hashed is correct or not and later deletes that folder.

---
+++ tests/test_project/tests/test_storage.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import tempfile
+
+from django.conf import settings
+from django.contrib.staticfiles import storage
+from django.contrib.staticfiles.management.commands.collectstatic import (
+    Command as CollectstaticCommand,
+)
+from django.core.management import call_command
+from django.test import TestCase, override_settings
+
+
+@override_settings(
+    STATICFILES_STORAGE='openwisp_utils.storage.CompressStaticFilesStorage',
+    STATIC_ROOT=os.path.join(settings.BASE_DIR, 'test_static_root'),
+    STATICFILES_FINDERS=['django.contrib.staticfiles.finders.FileSystemFinder',],
+    OPENWISP_STATICFILES_VERSIONED_EXCLUDE=['*skip_this.txt'],
+)
+class TestCompressStaticFilesStorage(TestCase):
+    def setUp(self):
+        temp_dir = tempfile.mkdtemp()
+        os.makedirs(os.path.join(temp_dir, 'test'))
+
+        self.file1 = os.path.join(temp_dir, 'test', 'skip_this.txt')
+        with open(self.file1, 'w') as f:
+            f.write('this will not be hashed')
+
+        self.file2 = os.path.join(temp_dir, 'test', 'this.txt')
+        with open(self.file2, 'w') as f:
+            f.write('this will be hashed')
+
+        self.patched_settings = self.settings(STATICFILES_DIRS=[temp_dir])
+        self.patched_settings.enable()
+        self.addCleanup(shutil.rmtree, temp_dir)
+
+    def tearDown(self):
+        shutil.rmtree(settings.STATIC_ROOT)
+
+    def test_hashed_name(self):
+        call_command('collectstatic')
+        hashed_files = storage.staticfiles_storage.hashed_files
+        self.assertEqual(hashed_files['test/skip_this.txt'], 'test/skip_this.txt')
+        self.assertNotEqual(hashed_files['test/this.txt'], 'test/this.txt')
