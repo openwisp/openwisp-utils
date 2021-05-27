34394738f78b42d0016573a4243cf747725f76f8
niteshsinha17 <niteshsinha1707@gmail.com>
2021-05-27 21:57:11 +0530

    [qa] Added linting of JS and CSS #173
    closes #173

--- openwisp_utils/admin_theme/static/admin/js/menu.js
+++ openwisp_utils/admin_theme/static/admin/js/menu.js
@@ -1,19 +1,21 @@
-window.addEventListener('load', function(){
-  'use strict';
-  var nav = document.getElementById('main-menu'),
-      container = document.getElementById('container'),
-      header = document.getElementById('header'),
-  setHeightToMenu = function() {
-    var windowHeight = window.innerHeight,
-        pageHeight = document.body.scrollHeight || container.offsetHeight,
-        height = windowHeight;
-    if (pageHeight > windowHeight) {
-      height = pageHeight;
-    }
-    if (!nav) { return; }
-    nav.style.height = (height - header.offsetHeight) + 'px';
-    nav.style.marginTop = header.offsetHeight + 'px';
-  };
-  setHeightToMenu();
-  window.addEventListener('resize', setHeightToMenu);
-});
+'use strict';
+(function () {
+  window.addEventListener('load', function(){
+    var nav = document.getElementById('main-menu'),
+        container = document.getElementById('container'),
+        header = document.getElementById('header'),
+    setHeightToMenu = function() {
+      var windowHeight = window.innerHeight,
+          pageHeight = document.body.scrollHeight || container.offsetHeight,
+          height = windowHeight;
+      if (pageHeight > windowHeight) {
+        height = pageHeight;
+      }
+      if (!nav) { return; }
+      nav.style.height = (height - header.offsetHeight) + 'px';
+      nav.style.marginTop = header.offsetHeight + 'px';
+    };
+    setHeightToMenu();
+    window.addEventListener('resize', setHeightToMenu);
+  });
+})();
--- openwisp_utils/static/openwisp-utils/js/receive_url.js
+++ openwisp_utils/static/openwisp-utils/js/receive_url.js
@@ -1,5 +1,5 @@
+'use strict';
 django.jQuery(function ($) {
-   "use strict";
    var p = $('.field-receive_url p, .field-receive_url > div > div'),
       value = p.text();
    p.html('<input readonly id="id_receive_url" type="text" class="vTextField readonly" value="' + value + '">');
