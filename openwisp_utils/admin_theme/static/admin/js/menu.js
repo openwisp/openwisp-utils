070ab016806f15084b637ba20f02f0ce4f1aacce
Nitesh Sinha <niteshsinha1707@gmail.com>
2021-06-14 13:26:11 +0530

    [ux] Create menu design

--- openwisp_utils/admin_theme/static/admin/js/menu.js
+++ openwisp_utils/admin_theme/static/admin/js/menu.js
@@ -1,37 +1,45 @@
+'use strict';
 (function($) {
-    'use strict';
-    var openMenu = localStorage.getItem('ow-menu');
-    if (window.innerWidth > 768) {
-        if(openMenu === null){
-            localStorage.setItem('ow-menu', true);
-        } else if (openMenu === 'false') {
-            $('#container').toggleClass('toggle-menu');
-        }
-    }
-    setTimeout(function () {
-        $('#menu').css('transition-duration', '0.3s');
-        $('#main-content').css('transition-duration', '0.3s');
-    }, 1000);
-    function toggleMenuHandler(){
-        $('#container').toggleClass('toggle-menu');
-        var currValue = localStorage.getItem('ow-menu');
-        if (window.innerWidth > 768) {
-            if (currValue === 'false') {
-                currValue = true;
-            } else {
-                currValue = false;
-            }
-            localStorage.setItem('ow-menu', currValue);
-        }
-    }
-    $('.heading').on('click', function (e) {
-        e.stopPropagation();
-        var heading = e.target;
-        ($(heading).parent()).toggleClass('active');
-    });
-    $('.menu-toggle').on('click', toggleMenuHandler);
-    $('#header-nav .user').on('click', toggleMenuHandler);
-    $('.backdrop, .menu-close').on('click', function () {
-        $('#container').toggleClass('toggle-menu');
-    });
+  setMenu($);
+  initMenuGroupClickListner($);
+  initToggleMenuHandlers($);
 })(django.jQuery);
+
+function initMenuGroupClickListner($) {
+  $('.menu-group-title').on('click', function (e) {
+    e.stopPropagation();
+    var groupTitle = e.target;
+    ($(groupTitle).parent()).toggleClass('active');
+  });
+}
+
+function setMenu($) {
+  var openMenu = localStorage.getItem('ow-menu');
+  if (window.innerWidth > 1024) {
+    if(openMenu === null){
+      localStorage.setItem('ow-menu', true);
+    } else if (openMenu === 'false') {
+      $('#container').toggleClass('toggle-menu');
+    }
+  }
+  setTimeout(function () {
+    $('#menu').css('transition-duration', '0.3s');
+    $('#main-content').css('transition-duration', '0.3s');
+  }, 1000);
+}
+
+function initToggleMenuHandlers($) {
+  function toggleMenuHandler(){
+    $('#container').toggleClass('toggle-menu');
+    var isMenuOpen = localStorage.getItem('ow-menu');
+    if (window.innerWidth > 1024) {
+      if (isMenuOpen === 'false') {
+        isMenuOpen = true;
+      } else {
+        isMenuOpen = false;
+      }
+      localStorage.setItem('ow-menu', isMenuOpen);
+    }
+  }
+  $('.menu-toggle').on('click', toggleMenuHandler);
+}
