c2d429845eac0d707cc4b837c55f3ed9c2364552
Federico Capoano <f.capoano@openwisp.io>
2021-08-10 12:27:15 -0500

    [fix] Added swapper to requirements
    We forgot to add it when adding the dashboard feature.

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     install_requires=[
         'django-model-utils>=4.0.0,<4.1.0',
         'django-compress-staticfiles~=1.0.1b',
+        'swapper>=1.1.2,<1.3.0',
     ],
     extras_require={
         'qa': [
