e49737869597ece9500736e766d5f24ad1e60d47
Federico Capoano <f.capoano@openwisp.io>
2021-11-29 16:03:29 -0300

    [change] Updated swapper~=1.3.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     install_requires=[
         'django-model-utils~=4.2.0',
         'django-compress-staticfiles~=1.0.1b',
-        'swapper>=1.1.2,<1.3.0',
+        'swapper~=1.3.0',
     ],
     extras_require={
         'qa': [
