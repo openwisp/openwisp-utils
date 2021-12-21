fa1c7166920d69dcc53d3edba5c1d5e29c2ffe07
Gagan Deep <pandafy.dev@gmail.com>
2021-12-21 23:06:15 +0530

    [chores] Improvements to templates and urls

--- tests/test_project/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-def non_admin_view(request):
-    # To test menu on non admin page
-    return render(request, 'test_project/non_admin_test.html')
--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
-from test_project.views import non_admin_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test-non-admin/', non_admin_view, name='test_non_admin'),
     path('api/v1/', include('openwisp_utils.api.urls')),
     path('api/v1/', include('test_project.api.urls')),
+    # Only used for tests
+    path(
+        'menu-test-view/',
+        TemplateView.as_view(template_name='test_project/menu_test.html'),
+        name='menu-test-view',
+    ),
 ]
