fdacdfe23b6ffe091688d4ab6a7d54d607378ab3
Sankalp <sankalp123427@gmail.com>
2022-07-09 02:21:50 +0530

    [change] Fixed multiple filter and added All placeholder

--- openwisp_utils/admin_theme/static/admin/js/ow-auto-filter.js
+++ openwisp_utils/admin_theme/static/admin/js/ow-auto-filter.js
@@ -2,7 +2,7 @@
 django.jQuery(document).ready(function () {
   // unbinding default event handlers of admin_auto_filters
   django.jQuery("#changelist-filter select, #grp-filters select").off("change");
-
+  django.jQuery(".auto-filter .select2-selection__placeholder").text("All");
   django.jQuery(".auto-filter").on("select2:open", function (event) {
     var optionsContainer = django
         .jQuery(event.target)
@@ -10,7 +10,9 @@ django.jQuery(document).ready(function () {
         .parent()
         .siblings()
         .last(),
-      dropDownContainer = django.jQuery(".select2-container--open")[1];
+      dropDownContainer = django
+        .jQuery(optionsContainer)
+        .find(".select2-container--open")[1];
     django.jQuery(optionsContainer).css("min-height", "14.75rem");
     django.jQuery(dropDownContainer).appendTo(optionsContainer);
     django.jQuery(dropDownContainer).removeAttr("style");
@@ -21,15 +23,15 @@ django.jQuery(document).ready(function () {
   });
 
   function applyFilter(target) {
-    var applyFilterButton = django.jQuery("#ow-apply-filter"); 
-    var filterElement = django.jQuery(target);
-    var val = filterElement.val() || "";
-    var class_name = filterElement.attr("class");
-    var param = filterElement.attr("name");
+    var applyFilterButton = django.jQuery("#ow-apply-filter"),
+      filterElement = django.jQuery(target),
+      val = filterElement.val() || "",
+      param = filterElement.attr("name"),
+      class_name = filterElement.attr("class");
     /* jshint -W117 */
     var filterQuery = search_replace(param, val);
     /* jshint +W117 */
-    if (applyFilterButton.length === 0) {
+    if (!applyFilterButton.length) {
       if (class_name.includes("admin-autocomplete")) {
         window.location.search = filterQuery;
       }
