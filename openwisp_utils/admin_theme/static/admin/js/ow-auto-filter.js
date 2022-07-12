7791e83904f2f722a355630d7b31090c94cc7025
Sankalp <sankalp123427@gmail.com>
2022-07-12 15:36:16 +0530

    [change] Fixed placeholder show and filter apply on select2:clear events

--- openwisp_utils/admin_theme/static/admin/js/ow-auto-filter.js
+++ openwisp_utils/admin_theme/static/admin/js/ow-auto-filter.js
@@ -2,7 +2,23 @@
 django.jQuery(document).ready(function () {
   // unbinding default event handlers of admin_auto_filters
   django.jQuery("#changelist-filter select, #grp-filters select").off("change");
-  django.jQuery(".auto-filter .select2-selection__placeholder").text("All");
+  function setAllPlaceholder(target = null) {
+    var allPlaceholder = gettext("All"),
+      placeholderSelector = ".select2-selection__placeholder";
+    if (target) {
+      // using setTimeout to execute this after internal select2 events
+      setTimeout(function () {
+        django
+          .jQuery(target)
+          .parent()
+          .find(placeholderSelector)
+          .text(allPlaceholder);
+      }, 100);
+    } else {
+      django.jQuery(`.auto-filter ${placeholderSelector}`).text(allPlaceholder);
+    }
+  }
+  setAllPlaceholder();
   django.jQuery(".auto-filter").on("select2:open", function (event) {
     var optionsContainer = django
         .jQuery(event.target)
@@ -36,6 +52,7 @@ django.jQuery(document).ready(function () {
         window.location.search = filterQuery;
       }
     } else {
+      django.jQuery(target).find(".filter-options").remove();
       django.jQuery(target).append(
         `<div class="filter-options">
           <a class="selected" href="${filterQuery}"></a>
@@ -49,6 +66,7 @@ django.jQuery(document).ready(function () {
   });
 
   django.jQuery(".auto-filter").on("select2:clear", function (event) {
+    setAllPlaceholder(event.target);
     applyFilter(event.target);
   });
 });
