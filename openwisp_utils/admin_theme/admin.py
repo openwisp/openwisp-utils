4b9c3f38d0615818dbcff8fe0eab8a5f84ca5000
Gagan Deep <pandafy.dev@gmail.com>
2022-10-27 19:01:40 +0530

    [feature] Added setting to make AutocompleteFilter view configurable
    Added "OPENWISP_AUTOCOMPLETE_FILTER_VIEW" project setting to allow
    configuring the view.

--- openwisp_utils/admin_theme/admin.py
+++ openwisp_utils/admin_theme/admin.py
@@ -3,11 +3,11 @@ import logging
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from django.utils.module_loading import import_string
 from django.utils.translation import gettext_lazy
 
 from . import settings as app_settings
 from .dashboard import get_dashboard_context
-from .views import AutocompleteJsonView
 
 logger = logging.getLogger(__name__)
 
@@ -31,10 +31,11 @@ class OpenwispAdminSite(admin.AdminSite):
         return super().index(request, extra_context=context)
 
     def get_urls(self):
+        autocomplete_view = import_string(app_settings.AUTOCOMPLETE_FILTER_VIEW)
         return [
             path(
                 'ow-auto-filter/',
-                self.admin_view(AutocompleteJsonView.as_view(admin_site=self)),
+                self.admin_view(autocomplete_view.as_view(admin_site=self)),
                 name='ow-auto-filter',
             ),
         ] + super().get_urls()
