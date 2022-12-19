a3855702854a34bfcea0f0aba83241ec6e6a52e3
Gagan Deep <pandafy.dev@gmail.com>
2022-12-19 21:50:33 +0530

    [enhancement] Added hooks to AutocompleteJsonView

--- openwisp_utils/admin_theme/views.py
+++ openwisp_utils/admin_theme/views.py
@@ -6,6 +6,12 @@ from django.http import JsonResponse
 class AutocompleteJsonView(BaseAutocompleteJsonView):
     admin_site = None
 
+    def get_empty_label(self):
+        return '-'
+
+    def get_allow_null(self):
+        return True
+
     def get(self, request, *args, **kwargs):
         (
             self.term,
@@ -22,13 +28,19 @@ class AutocompleteJsonView(BaseAutocompleteJsonView):
         context = self.get_context_data()
         # Add option for filtering objects with None field.
         results = []
+        empty_label = self.get_empty_label()
         if (
             getattr(self.source_field, 'null', False)
+            and self.get_allow_null()
             and not getattr(self.source_field, '_get_limit_choices_to_mocked', False)
             and not self.term
-            or self.term == '-'
+            or self.term == empty_label
         ):
-            results += [{'id': 'null', 'text': '-'}]
+            # The select2 library requires data in a specific format
+            # https://select2.org/data-sources/formats.
+            # select2 does not render option with blank "id" (i.e. '').
+            # Therefore, "null" is used here for "id".
+            results += [{'id': 'null', 'text': empty_label}]
         results += [
             {'id': str(obj.pk), 'text': self.display_text(obj)}
             for obj in context['object_list']
