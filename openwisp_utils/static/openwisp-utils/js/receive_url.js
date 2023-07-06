153c45c270b4178d2c1b1b50eed71135020b9fcd
Gagan Deep <pandafy.dev@gmail.com>
2023-07-06 22:42:14 +0530

    [chores] Fixed changes in theme due to Django 4.2.0
    - Fixed receive URL field
    - Fixed form row labels

--- openwisp_utils/static/openwisp-utils/js/receive_url.js
+++ openwisp_utils/static/openwisp-utils/js/receive_url.js
@@ -1,6 +1,6 @@
 'use strict';
 django.jQuery(function ($) {
-   var p = $('.field-receive_url p, .field-receive_url > div > div'),
+   var p = $('.field-receive_url p, .field-receive_url div.readonly'),
       value = p.text();
    p.html('<input readonly id="id_receive_url" type="text" class="vTextField readonly" value="' + value + '">');
    var field = $('#id_receive_url');
