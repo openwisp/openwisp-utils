bef90333d1f24e1cb635e80afd321b39f0fe60cf
dee077 <deepanshu.sahu27@gmail.com>
2025-06-04 17:18:35 +0530

    [fix] Update to double quotes and test channels fix

--- tests/openwisp2/asgi.py
+++ tests/openwisp2/asgi.py
@@ -2,5 +2,5 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openwisp2.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "openwisp2.settings")
 application = get_asgi_application()
