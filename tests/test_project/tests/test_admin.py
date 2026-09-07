5e9eb76c81682c0732e80a3eb33f98317a72ebfd
Federico Capoano <f.capoano@openwisp.io>
2026-09-07 13:36:09 -0300

    [chores] Addressed review feedback

--- tests/test_project/tests/test_admin.py
+++ tests/test_project/tests/test_admin.py
@@ -704,6 +704,10 @@ class TestAdmin(AdminTestMixin, CreateMixin, TestCase):
         "openwisp_utils.admin_theme.system_info.settings.INSTALLED_APPS",
         ["openwisp_users", "openwisp_utils.admin_theme"],
     )
+    @patch(
+        "openwisp_utils.admin_theme.system_info.get_installed_openwisp_packages",
+        return_value={"openwisp-utils": "1.4.0", "netjsonconfig": "1.3.0"},
+    )
     def test_system_information(self, *args):
         def _assert_system_information(response):
             self.assertContains(response, "<li>openwisp-utils:")
